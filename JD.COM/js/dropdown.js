90ef562b6266f09fbac64be81bf99c332c32f188
baoyuzhang <baoyuzhang@126.com>
2017-06-06 17:09:15 +0800

    CSS版本和JS版本分离，JS版本为主版本

--- JD.COM/js/dropdown.js
+++ JD.COM/js/dropdown.js
@@ -1,4 +1,6 @@
+var shortcut = document.getElementById('shortcut');
 var dropdowns = document.getElementsByClassName('dropdown');
+var mobile_jd = document.getElementsByClassName('mobile-jd')[0];
 
 for (var i = 0; i < dropdowns.length; i++) {
 	dropdowns[i].onmouseover = function(event) {
@@ -8,6 +10,12 @@ for (var i = 0; i < dropdowns.length; i++) {
 		removeClass(this,'dropdown_hover');
 	};
 }
+mobile_jd.onmouseover = function(event) {
+	addClass(this,'mobile-jd_hover');
+};
+mobile_jd.onmouseout = function(event) {
+	removeClass(this,'mobile-jd_hover');
+};
 
 function hasClass( elements,cName ){ 
 	return !!elements.className.match( new RegExp( "(\\s|^)" + cName + "(\\s|$)") ); // ( \\s|^ ) 判断前面是否有空格 （\\s | $ ）判断后面是否有空格 两个感叹号为转换为布尔值 以方便做判断 
